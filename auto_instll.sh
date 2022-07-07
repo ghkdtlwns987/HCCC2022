d70891e305862ccf262c3062040bf368823fccb8
ghkdtlwns987 <54488922+ghkdtlwns987@users.noreply.github.com>
2022-07-07 11:49:04 +0900

    Update auto_instll.sh

--- auto_instll.sh
+++ auto_instll.sh
@@ -1,30 +1,28 @@
 apt update -y
 apt install -y apache2
-apt intsall -y mysql-server
+apt install -y mysql-server
+apt install -y mysql-client
 apt install -y vim
 apt install -y php
 apt install -y net-tools
 apt install php-mysqli
-apt install -y mysql-server
-apt install -y mysql-client
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
-
 
 rm /etc/apache2/apache2.conf
-cp ~/hccc2022_site/setup/apache2.conf /etc/apache2/apache2.conf
+cp ~/HCCC2022/hccc2022_site/setup/apache2.conf /etc/apache2/apache2.conf
 
 rm /etc/php/7.4/cli/php.ini
-mv ~/hccc2022_site/setup/php.ini /etc/php/7.4/cli/php.ini
+cp ~/HCCC2022/hccc2022_site/setup/php.ini /etc/php/7.4/cli/php.ini
 
 mysql -uroot -p <<EOFMYSQL
-source ~/hccc2022_site/setup/schema.sql
+source ~/HCCC2022/hccc2022_site/setup/schema.sql
 show databases;
 EOFMYSQL
 
 rm -rf /var/www/html/*
-mv ~/hccc2022_site/ /var/www/html/
+cp ~/HCCC2022/hccc2022_site/html/* /var/www/html/
+
+chown -R www-data:www-data /var/www/html    # apache2 권한 제한
+chmod -R 777 /var/www/html/
 
 service apache2 restart
 service mysql restart
